3bd39f0a3d6adb8c68d10ce04ad7803a3c959006
gabrielpaoli <gabrielpaoli@hotmail.com>
2020-02-03 10:15:41 -0300

    First commit

---
+++ src/app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import TasksForm from './TasksForm';
+import Pokedex from './Pokedex';
+
+//render(<TasksForm/>, document.getElementById('taskForm')); 
+
+render(<Pokedex/>, document.getElementById('pokedex')); 
